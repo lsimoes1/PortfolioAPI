6bfbb234a3df285061426678f86c12c6814233f7
Luan Simões <luan.simoes1@gmail.com>
2021-02-03 13:25:19 -0300

    Inserção do Cors na rota Academy

--- Portfolio.API/Controllers/AcademyController.cs
+++ Portfolio.API/Controllers/AcademyController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Portfolio.API.Business.Interface;
 using Portfolio.API.Model.Response;
 
 namespace Portfolio.API.Controllers
 {
+    [EnableCors]
+    [Authorize("Bearer")]
     [Route("api/[controller]")]
     public class AcademyController : Controller
     {
